f3ae26fe974b2a99c717380e20be91d1257287b9
Zack Kuptsow <zkups@udel.edu>
2021-05-14 14:11:31 -0400

    Delete credentials.py

--- spotify/credentials.py
+++
@@ -1,4 +0,0 @@
-CLIENT_ID = "7e93a92d82464cb98ff7dc8760c56b0d"
-CLIENT_SECRET = "b62700b5fe3b403aa24205e875f60865"
-# need to add to Settings on Spotify Dev Dashboard in order to work
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
